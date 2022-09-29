acfb59eaa5d3fc820cb6f4526909d03c910493f3
ag <gratzer.andreas@gmail.com>
2022-09-29 14:44:34 +0200

    .

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # nix_home_manager
 
-
 	export NIX_PATH=${NIX_PATH:+$NIX_PATH:}$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels
 	export XDG_DATA_DIRS="/home/your_user/.nix-profile/share:$XDG_DATA_DIRS"
 
--- scripts/install_nix_hm.sh
+++ scripts/install_nix_hm.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --update
-
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
 nix-channel --update
 
+https://nix-community.github.io/home-manager/index.html
 export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
 nix-shell '<home-manager>' -A install
 
+ln -s $PWD../ ~/.config/nixpkgs
 
-ln -s $PWD/home.nix ~/.config/nixpkgs
-ln -s $PWD/config.nix ~/.config/nixpkgs
 
